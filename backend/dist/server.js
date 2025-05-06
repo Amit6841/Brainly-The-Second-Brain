aac69920fb54601fc8c8faadb752d64c2881d816
Amit kumar pallai <159411434+Amit6841@users.noreply.github.com>
2025-05-06 14:29:24 +0530

    Update server.js

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -22,7 +22,7 @@ app.use(body_parser_1.default.urlencoded({
     extended: true,
 }));
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: "http://localhost:5173", credentials: true }));
+app.use((0, cors_1.default)({ origin: "https://brainly-the-second-brain-client.vercel.app", credentials: true }));
 app.use("/api/user", userRoutes_1.default);
 app.use("/api/content", contentRoutes_1.default);
 app.use("/api/brain", shareRoutes_1.default);
